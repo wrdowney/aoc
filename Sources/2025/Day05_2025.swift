7ac067db03dc995849932aac13df7888a7111faf
wrdowney <me@wrdowney.com>
2025-12-10 08:46:54 -0500

    feat: speedup day 05 p1

--- Sources/2025/Day05_2025.swift
+++ Sources/2025/Day05_2025.swift
@@ -2,14 +2,30 @@ import Foundation
 
 struct Day05_2025: AdventOfCodeDay {
     let title = "Cafeteria"
-    let freshIDs: [ClosedRange<Int>]
+    var freshIDs: Set<ClosedRange<Int>>
     let ids: [Int]
 
     init(input: String) {
         let inputs = input.lines.split(separator: "")
-        freshIDs = inputs[0].map {
+        var ranges = inputs[0].map {
             let components = $0.components(separatedBy: "-")
             return Int(components[0])!...Int(components[1])!
+        }.sorted { $0.lowerBound < $1.lowerBound }
+        
+        freshIDs = .init()
+
+        // merge ranges
+        while !ranges.isEmpty {
+            let first = ranges.removeFirst()
+            guard let second = ranges.first else {
+                freshIDs.insert(first)
+                break
+            }
+            if first.overlaps(second) {
+                ranges[0] = first.lowerBound ... max(first.upperBound, second.upperBound)
+            } else {
+                freshIDs.insert(first)
+            }
         }
         
         ids = inputs[1].map {
@@ -18,37 +34,16 @@ struct Day05_2025: AdventOfCodeDay {
     }
 
     func part1() async -> Int {
-        var result = 0
-        for id in ids {
+        return ids.filter{ id in
             for range in freshIDs {
-                if range.contains(id) {
-                    result += 1
-                    break
-                }
+                if range.contains(id) { return true }
             }
-        }
-        return result
+            return false
+        }.count
     }
 
     func part2() async -> Int {
-        var ranges = freshIDs.sorted { $0.lowerBound < $1.lowerBound }
-        var result = Set<ClosedRange<Int>>()
-
-        // merge ranges
-        while !ranges.isEmpty {
-            let first = ranges.removeFirst()
-            guard let second = ranges.first else {
-                result.insert(first)
-                break
-            }
-            if first.overlaps(second) {
-                ranges[0] = first.lowerBound ... max(first.upperBound, second.upperBound)
-            } else {
-                result.insert(first)
-            }
-        }
-
-        return result
+        return freshIDs
             .map { $0.upperBound - $0.lowerBound + 1}
             .reduce(0, +)
 
